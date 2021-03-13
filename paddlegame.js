afac60c5b30e46e55b4d956efa470c1791512f16
Tom Black <tom.black06007@gmail.com>
2021-03-12 18:46:13 -0700

    Created the ball for the paddle game.

--- paddlegame.js
+++ paddlegame.js
@@ -3,7 +3,13 @@ let canvas, canvasContext;
 window.onload = function () {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-
+    //Create the background
     canvasContext.fillStyle = "black";
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    //Create the ball
+    canvasContext.fillStyle = "white";
+    canvasContext.beginPath();
+    canvasContext.arc(100,100,10, 0, Math.PI*2, true);
+    canvasContext.fill();
+
 }
