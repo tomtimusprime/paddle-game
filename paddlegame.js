e0d846e6b405ab96d9dae12f4ce679db7264b14b
Tom Black <tom.black06007@gmail.com>
2021-03-12 18:52:59 -0700

    Added motion to the ball.

--- paddlegame.js
+++ paddlegame.js
@@ -1,15 +1,24 @@
 let canvas, canvasContext;
+let ballX = 75;
 
 window.onload = function () {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
+    //Create motion for the ball
+    let framesPerSecond = 30;
+    setInterval(updateAll, 1000 / framesPerSecond);
+
+}
+
+const updateAll = () => {
+    ballX++;
     //Create the background
     canvasContext.fillStyle = "black";
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
     //Create the ball
     canvasContext.fillStyle = "white";
     canvasContext.beginPath();
-    canvasContext.arc(100,100,10, 0, Math.PI*2, true);
+    canvasContext.arc(ballX, 100, 10, 0, Math.PI * 2, true);
     canvasContext.fill();
 
 }
