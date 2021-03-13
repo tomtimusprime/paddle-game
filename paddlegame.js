a44caec870b3718f7eadc0bb639b6af0076e1132
Tom Black <tom.black06007@gmail.com>
2021-03-12 18:40:43 -0700

    Cleaned up the code a bit and created a separate javascript file to house the game in.

---
+++ paddlegame.js
@@ -0,0 +1,9 @@
+let canvas, canvasContext;
+
+window.onload = function () {
+    canvas = document.getElementById('gameCanvas');
+    canvasContext = canvas.getContext('2d');
+
+    canvasContext.fillStyle = "black";
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+}
